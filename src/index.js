b63ca3c1c79d0330db4cd48cf412e84296e4dae4
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-03-26 19:52:17 -0700

    Dropdown example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./style.css";
+
+document.addEventListener("click", (e) => {
+  const link = e.target.matches("[data-dropdown-link]");
+
+  if (!link && e.target.closest("[data-dropdown]") !== null) return;
+
+  let currentDropDown;
+  if (link) {
+    currentDropDown = e.target.closest("[data-dropdown]");
+    currentDropDown.classList.toggle("show");
+  }
+
+  document.querySelectorAll("[data-dropdown].show").forEach((dropdown) => {
+    if (dropdown === currentDropDown) return;
+    dropdown.classList.remove("show");
+  });
+});
